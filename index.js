cb4496dbe60e630b432ef2baf4746bf5beec8d3d
Mazharul Islam <swopnil420@gmail.com>
2022-02-13 23:44:05 +0600

    Storage and remove items from localstorage

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 const selectForm = document.querySelector('form.todoForm');
 const selectItem = document.querySelector('ul.listItems');
 
-selectItem.addEventListener('click', handleItemClick);
+loadListeres();
+
+function loadListeres() {
+	// Get items from local store
+	document.addEventListener('DOMContentLoaded', getListFromLocalStorage);
+
+	selectItem.addEventListener('click', handleItemClick);
+
+	selectForm.addEventListener('submit', handleSubmit);
+}
+
+/**
+ * Get List from local storage
+ */
+function getListFromLocalStorage() {
+	let lists;
+
+	if (localStorage.getItem('lists') === null) {
+		lists = [];
+	} else {
+		lists = JSON.parse(localStorage.getItem('lists'));
+	}
+
+	lists.forEach((list) => {
+		const addNewItem = document.createElement('li');
+		addNewItem.textContent = list;
+		addNewItem.insertAdjacentElement('afterbegin', removeButton());
+		return selectItem.appendChild(addNewItem);
+	});
+}
+
+/**
+ * TO DO items store in local storage
+ * @param {*} item
+ */
+function storeListInLocalStorage(item) {
+	let lists;
+
+	if (localStorage.getItem('lists') === null) {
+		lists = [];
+	} else {
+		lists = JSON.parse(localStorage.getItem('lists'));
+	}
+
+	lists.unshift(item);
+	localStorage.setItem('lists', JSON.stringify(lists));
+}
+
+/**
+ * Handle single item
+ * @param {*} e
+ */
 
 function handleItemClick(e) {
 	if (e.target.tagName === 'LI') {
@@ -13,11 +64,18 @@ function handleItemClick(e) {
 	}
 
 	if (e.target.parentNode.classList.contains('remove')) {
-		e.target.parentNode.parentNode.remove();
+		if (confirm('Are you sure?')) {
+			e.target.parentNode.parentNode.remove();
+			removeListFromLocalStorage(e.target.parentNode.parentNode);
+		}
 	}
 }
 
-selectForm.addEventListener('submit', handleSubmit);
+/**
+ * Handle input item and store to locastorage
+ * after input get empty field
+ * @param {*} e
+ */
 
 function handleSubmit(e) {
 	e.preventDefault();
@@ -30,9 +88,17 @@ function handleSubmit(e) {
 			'afterbegin',
 			newItem(e.target.name.value)
 		);
+		storeListInLocalStorage(input); // store item in local storage
 		e.target.name.value = ''; // empty field after putting contnet
 	}
 }
+
+/**
+ * form validation
+ * @param {*} input
+ * @param {*} element
+ * @returns
+ */
 
 function validateInput(input, element) {
 	if (input) {
@@ -44,23 +110,54 @@ function validateInput(input, element) {
 	}
 }
 
+/**
+ * add new item
+ * @param {*} content : ;
+ * @returns
+ */
 function newItem(content) {
 	const addNewItem = document.createElement('li'); // create new item
 	addNewItem.textContent = content; // input content
 	addNewItem.insertAdjacentElement('afterbegin', removeButton()); // add item one after one
-
 	return addNewItem;
 }
 
+/**
+ * handle remove icon
+ * @returns
+ */
 function removeButton() {
 	const createRemoveBtn = document.createElement('span'); // Make remove btn
 	createRemoveBtn.classList.add('remove'); // add class
+
 	const deletebtn = document.createElement('img');
 	deletebtn.setAttribute('src', './delete.png');
 	deletebtn.style.width = '20px';
 	deletebtn.style.height = '20px';
 	createRemoveBtn.appendChild(deletebtn); // add content: ;
-	// createRemoveBtn.textContent = 'x';
 
 	return createRemoveBtn;
+}
+
+/**
+ * Remove item from local storage
+ * @param {*} listItem
+ */
+
+function removeListFromLocalStorage(listItem) {
+	let lists;
+
+	if (localStorage.getItem('lists') === null) {
+		lists = [];
+	} else {
+		lists = JSON.parse(localStorage.getItem('lists'));
+	}
+
+	lists.forEach((list, index) => {
+		if (listItem.textContent === list) {
+			lists.splice(index, 1);
+		}
+
+		localStorage.setItem('lists', JSON.stringify(lists));
+	});
 }
